2c01152d7747f699367ba30967a661a06e6e9bd8
Sennett Lau <laub1199@gmail.com>
2023-01-24 16:27:25 +0800

    added .gitignore, updated provider.tf with backend, removed deploy files

--- provider.tf
+++ provider.tf
@@ -4,10 +4,16 @@ terraform {
       source = "hashicorp/aws"
     }
   }
+
+  backend "s3" {
+    bucket = "foundation-terraform-state"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+  }
 }
 
 provider "aws" {
-  region                   = "eu-west-2"
+  region                   = "us-west-2"
   shared_credentials_files = ["~/.aws/credentials"]
 }
 
