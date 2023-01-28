2db1d1ecbfa7de12b258d32458e5c0bf955b1308
Sennett Lau <laub1199@gmail.com>
2023-01-29 01:31:49 +0800

    added sns module with related variables

---
+++ sns-variables.tf
@@ -0,0 +1,5 @@
+variable "support_email" {
+  description = "Email address to send support requests to"
+  type        = string
+  default     = "tech@example.com"
+}
--- variables.tf
+++ variables.tf
@@ -8,9 +8,3 @@ variable "az_count" {
     error_message = "AZCount must be less than or equal to 6 (the largest number of azs which is in us-east-1)."
   }
 }
-
-variable "support_email" {
-  description = "Support email address"
-  type        = string
-  default     = "tech@example.com"
-}
