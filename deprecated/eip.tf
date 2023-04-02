1a6ff17629b677a422f0f0dac42b0257eb820b5a
Sennett Lau <laub1199@gmail.com>
2023-04-02 23:43:22 +0800

    deleted unused code

--- deprecated/eip.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_eip" "eip" {
-  count      = min(length(data.aws_availability_zones.azs.names), var.az_count)
-  vpc        = true
-  depends_on = [aws_internet_gateway_attachment.attach_gateway]
-}
